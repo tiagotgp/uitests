042c7808a2db9544f2edb8effc4f3b6cfed6067d
tiagotgp <tiago_tgp_@hotmail.com>
2018-02-23 16:42:12 +0000

    Fix missing feature folder

---
+++ favourites-movie-appUITests/Features/search_movies_feature.swift
@@ -0,0 +1,30 @@
+//
+//  search_movies_feature.swift
+//  favourites-movie-app
+//
+//  Created by Goncalves Pereira, Tiago on 12/10/2017.
+//  Copyright © 2017 Goncalves Pereira, Tiago. All rights reserved.
+//
+
+import XCTest
+import XCTest_Gherkin
+import SBTUITestTunnel
+
+class search_movies_feature: Feature {
+    
+    private func background() {
+        Given("I'm viewing the search results section")
+    }
+    
+    func testViewResults() {
+        self.background()
+        When("I search for Star Wars")
+        Then("I should see search results")
+    }
+    
+    func testNoMoviesFound() {
+        self.background()
+        When("I make a search that yields no results")
+        Then("I should be notified that no movies were found")
+    }
+}

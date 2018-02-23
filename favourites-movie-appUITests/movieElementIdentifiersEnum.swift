f4611bf1b36561dcf4ee0cfe9618237301ea6568
tiagotgp <tiago_tgp_@hotmail.com>
2018-02-23 15:54:18 +0000

    Move element identifiers to static constants

---
+++ favourites-movie-appUITests/movieElementIdentifiersEnum.swift
@@ -0,0 +1,16 @@
+//
+//  movieElementsEnum.swift
+//  favourites-movie-appUITests
+//
+//  Created by Goncalves Pereira, Tiago (Tester) on 23/02/2018.
+//  Copyright © 2018 Goncalves Pereira, Tiago. All rights reserved.
+//
+
+import Foundation
+
+struct MovieElementIdentifiers {
+    static let movieTitle = "movieTitle"
+    static let movieYear = "movieYear"
+    static let movieImage = "movieImage"
+    static let favouriteButton = "favouriteButton"
+}

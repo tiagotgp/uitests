ecb356577ec29ffc1d209727707e89b735f290cd
tiagotgp <tiago_tgp_@hotmail.com>
2018-02-27 16:22:01 +0000

    Remove methods related to movies in the HasTableView protocol

--- favourites-movie-appUITests/HasTableView.swift
+++ favourites-movie-appUITests/HasTableView.swift
@@ -15,39 +15,10 @@ protocol HasTableView: class {
 
 extension HasTableView {
     
-    static func getMovieDetailsElement(index: UInt, elementIdentifier: String) -> String? {
-        return tableView?.cells.element(boundBy: index).staticTexts[elementIdentifier].label.description
-    }
-    
     static func getNumberOfCells() -> Int {
         
         guard let tableView = self.tableView else { return 0 }
         return Int(bitPattern: (tableView.cells.count))
     }
-    
-    static func getMovieCellIndexByDetails(title: String, year: String) -> UInt? {
-        
-        for movie in (0..<self.getNumberOfCells()) {
-            let number:UInt = UInt(movie)
-            if (getMovieDetailsElement(index: number, elementIdentifier: "movieTitle")) == title && getMovieDetailsElement(index: number, elementIdentifier: year) == year {
-                return number
-            }
-        }
-        
-        XCTFail("Cannot find movie: title: \(title) - year: \(year)")
-        return nil
-    }
-    
-    static func getMovieCellIndexByTitle(title: String) -> UInt? {
-        
-        for movie in (0..<self.getNumberOfCells()) {
-            let number:UInt = UInt(movie)
-            if (getMovieDetailsElement(index: number, elementIdentifier: "movieTitle")) == title {
-                return number
-            }
-        }
-        
-        XCTFail("Cannot find movie: title: \(title)")
-        return nil
-    }
+
 }
